b1b7f14a2283c88048896bbf48d63d38b8d5e52f
ahmedgawish10 <ahmdgawish44@gmail.com>
2024-09-17 16:39:24 +0300

    updates my portfolio project

--- src/components/portfolio/Alldata.jsx
+++ src/components/portfolio/Alldata.jsx
@@ -1,25 +1,28 @@
 import React from "react";
 import iti from "../../assets/iti-ecommerce.png";
-import compny from "../../assets/portfolio-compny.png";
-import Portfolio from "../../assets/portfolio-angular.png";
 import EcomAngular from "../../assets/ecommerce-angular.png";
+import Eljewar from "../../assets/eljwewar-travels.png";
+import Courses from "../../assets/courses-app.png";
 
 import finalproject from "../../assets/final-project.png";
 import ecompharmacy from "../../assets/ecommerce-pharmacy-min.png";
 const Alldata = () => {
   return (
     <div className="portfolio__all-data">
+      {/* project one */}
       <div className="box">
         <div className="box__img">
-          <img src={iti} alt="E-commerce Pizza" />
+          <a href="https://ecommerce-iti.vercel.app/" target="_blank" alt="">
+            <img src={iti} alt="E-commerce Pizza" />
+          </a>
         </div>
         <div className="title__project">
-          <h3>    E-commerce Pizza</h3>
-          Graduation Project at - I T I
+          <h3 className="pb-2">    E-commerce Pizza</h3>
+          <span>          Graduation Project at - I T I          </span>
         </div>
-        <div>Technologies: (HTML - Sass - JS - BootStrap - Swiper.js) </div>
+        <div><span className="font-bold">Technologies </span>: (HTML - Sass - JS - BootStrap - Swiper.js) </div>
         <h5 className="pt-1">
- 
+
           <a
             href="https://ecommerce-iti.vercel.app/"
             target="_blank"
@@ -30,38 +33,93 @@ const Alldata = () => {
           <i className="uil uil-arrow-right services__button-icon"></i>
         </h5>
       </div>
+      {/* project two */}
       <div className="box">
-        <div className="box__img">
-          <img src={compny} alt="portfolio company" />
+        <div className="box__img-web">
+          <a href="https://aljewartours.vercel.app/" target="_blank" alt="">
+            <img src={Eljewar} alt="E-commerce Pizza" />
+          </a>
         </div>
         <div className="title__project">
-         <h3>Gawish Company</h3> 
-           company for Designs
+          <h3 className="pb-2">Eljewar travels App</h3>
+          <span>Freelance Project</span>
+          <div>Technologies: (HTML - CSS- JS - Tailwind -Nextjs - i18next ) </div>
+
         </div>
-        <div>Technologies: (HTML - CSS - JS - BootStrap - Isotope.js) </div>
-
-        <h5 className="pt-1">
-
+        <h5 className="pt-1 d-flex ">
           <a
-            href="https://ahmedgawish10.github.io/portfolio-company/"
+            href="https://aljewartours.vercel.app/"
             target="_blank" rel="noreferrer"
           >
-
             Demo
           </a>
-          <i className="uil uil-arrow-right services__button-icon"></i>{" "}
+          <i className="uil uil-arrow-right services__button-icon me-3"></i>
+
+
         </h5>
+
       </div>
+     {/* project three */}
+      <div className="box">
+        <div className="box__img-web">
+          <a href="https://fullstack-courses-app-3vdz.vercel.app/" target="_blank" alt="">
+            <img src={Courses} alt="E-commerce Pizza" />
+          </a>
+        </div>
+        <div className="title__project">
+          <h3 className="pb-2">Courses Tehc App</h3>
+          <span>Training Project</span>
+          <div>Technologies: (HTML - CSS- JS - Tailwind -Nextjs - Strapi(CMS) - Stripe - ClerkAuth - Cloudinary ) </div>
+
+        </div>
+        <h5 className="pt-1 d-flex ">
+          <a
+            href="https://aljewartours.vercel.app/"
+            target="_blank" rel="noreferrer"
+          >
+            Demo
+          </a>
+          <i className="uil uil-arrow-right services__button-icon me-3"></i>
+
+
+        </h5>
+
+      </div>
+     {/* project four */}
       <div className="box">
         <div className="box__img-react">
-          <img src={finalproject} alt="" />
+          <a href="https://ecommerce-pharmacy4.vercel.app/" target="_blank" alt="">
+            <img src={ecompharmacy} alt="E-commerce Pizza" />
+          </a>
+        </div>
+        <h3 className="title__project">E-commerce Pharmacy</h3>
+        <div className="technologies">
+          Technologies: (HTML - CSS - BootStrap - React - ReduxToolkit
+          -(react-router-dom) - (slick-carousel))
+        </div>
+        <h5 className="pt-1">
+          <a
+            href="https://ecommerce-pharmacy4.vercel.app/"
+            target="_blank" rel="noreferrer"
+          >
+            Demo <i className="uil uil-arrow-right services__button-icon"></i>
+
+          </a>
+        </h5>
+      </div>
+    {/* project five */}
+      <div className="box">
+        <div className="box__img-react">
+          <a href="https://ahmedgawish10.github.io/graduateproject/" target="_blank" alt="">
+            <img src={finalproject} alt="E-commerce Pizza" />
+          </a>
         </div>
         <h3 className="title__project">Graduation Project</h3>
         <div>
-        Technologies: (HTML - CSS - BootStrap - React - Context api
+          Technologies: (HTML - CSS - BootStrap - React - Context api
           -(react-router-dom))
         </div>
-        <h5>
+        <h5 className="pt-1">
           <a
             href="https://ahmedgawish10.github.io/graduateproject/"
             target="_blank" rel="noreferrer"
@@ -71,47 +129,18 @@ const Alldata = () => {
           </a>
         </h5>
       </div>
-      <div className="box">
-            <div className="box__img-react">
-              <img src={ecompharmacy} alt="" />
-            </div>
-            <h3 className="title__project">E-commerce Pharmacy</h3>
-            <div className="technologies">
-            Technologies: (HTML - CSS - BootStrap - React - ReduxToolkit
-              -(react-router-dom) - (slick-carousel))
-            </div>
-            <h5>
-              <a
-                href="https://ecommerce-pharmacy4.vercel.app/"
-                target="_blank" rel="noreferrer"
-              >
-                Demo <i className="uil uil-arrow-right services__button-icon"></i>
-
-              </a>
-            </h5>
-          </div>
+    {/* project six */}
       <div className="box">
         <div className="box__img-angular">
-          <img src={Portfolio} alt="" />
-        </div>
-        <h3 className="title__project">Portfolio (Angular )</h3>
-        <div className="technologies">
-        Technologies: (HTML - CSS - JS -BootStrap - Angular )
-            </div>
-        <h5>
-          <a href="https://lab-angular-two.vercel.app/" target="_blank" rel="noreferrer"> Demo</a>
-          <i className="uil uil-arrow-right services__button-icon"></i>
-        </h5>
-      </div>
-      <div className="box">
-        <div className="box__img-angular">
-          <img src={EcomAngular} alt="" className="" />
+          <a href="https://ecommerce-angular-beta-lime.vercel.app/" target="_blank" alt="">
+            <img src={EcomAngular} alt="E-commerce Pizza" />
+          </a>
         </div>
         <h3 className="title__project">MediCare App (Angular)</h3>
         <div className="technologies">
-        Technologies: (HTML - CSS - JS -BootStrap - Angular - REST API )
-            </div>
-        <h5>
+          Technologies: (HTML - CSS - JS -BootStrap - Angular - REST API )
+        </div>
+        <h5 className="pt-1">
 
           <a href="https://ecommerce-angular-beta-lime.vercel.app/" target="_blank" rel="noreferrer"> Demo</a>
           <i className="uil uil-arrow-right services__button-icon"></i>
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,248 +3,212 @@ import Alldata from "./Alldata";
 import "./portfolio.css";
 import Image6 from "../../assets/iti-traning.jpg";
 import jopPortal from "../../assets/jop-portal.png";
-import Furniro from "../../assets/Furniro.png";
-
+import Eljewar from "../../assets/eljwewar-travels.png";
+import Courses from "../../assets/courses-app.png";
 import finalproject from "../../assets/final-project.png";
 import ecompharmacy from "../../assets/ecommerce-pharmacy-min.png";
 
 const Portfolio = () => {
+  const [port, setPort] = useState(1);
+  const [imgGrad, setImgGrad] = useState(false);
+
   window.addEventListener("scroll", () => {
     const boxImg = document.querySelectorAll(".box__img img");
-// animation fro section portfolio all
-    boxImg.forEach(element => {
-      if (window.scrollY >= 1800) {
-      element.classList.add("animate__img");
+    const boxImgReact = document.querySelectorAll(".box__img-react img");
+    const boxImgWeb = document.querySelectorAll(".box__img-web img");
 
-      }else {
-        if (boxImg) {
-         element.classList.remove("animate__img");
+    // Animation for all sections
+    const animateImages = (elements, className) => {
+      elements.forEach((element) => {
+        if (window.scrollY >= 1800) {
+          element.classList.add(className);
         } else {
-          return;
+          element.classList.remove(className);
         }
-      }
-     });
-// animation fro section portfolio react
-const boxImgReact = document.querySelectorAll(".box__img-react img");
-boxImgReact.forEach(element => {
-  if (window.scrollY >= 1800) {
-  element.classList.add("animate__img2");
-  }else {
-    if (boxImg) {
-     element.classList.remove("animate__img2");
-    } else {
-      return;
-    }
-  }
- });
-// animation fro section portfolio web
-const boxImgWeb = document.querySelectorAll(".box__img-web img");
-boxImgWeb.forEach(element => {
-  if (window.scrollY >= 1800) {
-  element.classList.add("animate__img-web");
-  }else {
-    if (boxImg) {
-     element.classList.remove("animate__img-web");
-    } else {
-      return;
-    }
-  }
- });
+      });
+    };
 
-    
+    animateImages(boxImg, "animate__img");
+    animateImages(boxImgReact, "animate__img2");
+    animateImages(boxImgWeb, "animate__img-web");
   });
 
-  const [port, setport] = useState(1);
-  const [imgGrad, setimgGrad] = useState(false);
   return (
-    <div className="portfolio section" id="portfolio o">
+    <div className="portfolio section" id="portfolio">
       <h2 className="section__title">Portfolio</h2>
       <span className="section__subtitle">Most projects</span>
 
       <div className="portfolio__container container grid">
+        {/* Portfolio Tabs */}
         <div className="portfolio__tabs">
           <button
             className={port === 1 ? "button-active" : "portolio__tabs-button"}
-            onClick={() => {
-              setport(1);
-            }}
+            onClick={() => setPort(1)}
           >
             All
           </button>
-
           <button
-            onClick={() => {
-              setport(2);
-            }}
             className={port === 2 ? "button-active" : "portolio__tabs-button"}
+            onClick={() => setPort(2)}
           >
             Web
           </button>
           <button
-            onClick={() => {
-              setport(3);
-            }}
             className={port === 3 ? "button-active" : "portolio__tabs-button"}
+            onClick={() => setPort(3)}
           >
             React
           </button>
           <button
-            onClick={() => {
-              setport(4);
-            }}
             className={port === 4 ? "button-active" : "portolio__tabs-button"}
+            onClick={() => setPort(4)}
           >
             Certificates
           </button>
         </div>
-        <div
-          className={port === 1 ? "portfolio__all-show" : "portfolio__all-hide"}
-        >
+
+        {/* All Projects */}
+        <div className={port === 1 ? "portfolio__all-show" : "portfolio__all-hide"}>
           <Alldata />
         </div>
-        {/* web */}
+
+        {/* Web Projects */}
         <div className={port === 2 ? "portfolio__web-show" : "portfolio__web-hide"}>
           <div className="box">
-            <div className="box__img-web ">
-              <img src={jopPortal} alt="" />
+            <div className="box__img-web">
+              <img src={jopPortal} alt="Job Portal App" />
             </div>
             <div className="title__project">
-             <h3>Job Portal</h3> 
-         <span>Graduation Project at - I T I</span> 
-        </div>
-        <div>Technologies: (HTML - Sass- JS - BootStrap - PHP - MY SQL) </div>
-        <h5 className="pt-1">
-          <a
-            href="https://github.com/Ahmedgawish10/JobPortal-app"
-            target="_blank" rel="noreferrer"
-          >
-            Code
-          </a>
-          <i className="uil uil-arrow-right services__button-icon"></i>
-        </h5>
-
-
+              <h3 className="pb-2">Job Portal App</h3>
+              <span>Graduation Project at - I T I</span>
+              <div>Technologies: (HTML - Sass - JS - BootStrap - PHP - MySQL)</div>
+              <h5 className="pt-1">
+                <a
+                  href="https://github.com/Ahmedgawish10/JobPortal-app"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Code
+                </a>
+                <i className="uil uil-arrow-right services__button-icon"></i>
+              </h5>
+            </div>
           </div>
+
           <div className="box">
             <div className="box__img-web">
-              <img src={Furniro} alt="" />
+              <img src={Eljewar} alt="Eljewar Travels App" />
             </div>
             <div className="title__project">
-            <h3 >Furniro App</h3>
-            <span>Graduation Project at - I T I</span> 
-            <div>Technologies: (HTML - CSS- JS - BootStrap -Angular - PHP - Laravel - API ) </div>
-
+              <h3 className="pb-2">Eljewar Travels App</h3>
+              <span>Freelance Project</span>
+              <div>Technologies: (HTML - CSS - JS - Tailwind - Next.js - i18next)</div>
+              <h5 className="pt-1 d-flex">
+                <a href="https://aljewartours.vercel.app/" target="_blank" rel="noreferrer">
+                  Demo
+                </a>
+                <i className="uil uil-arrow-right services__button-icon me-3"></i>
+              </h5>
             </div>
-            <h5 className="pt-1 d-flex ">
-          <a 
-            href="https://github.com/Ahmedgawish10/JobPortal-app"
-            target="_blank" rel="noreferrer"
-          >
-            Frontend code 
-          </a>
-          <i className="uil uil-arrow-right services__button-icon me-3"></i>
-   
-          <a
-            href="https://github.com/Ahmedgawish10/JobPortal-app"
-            target="_blank" rel="noreferrer"
-          >
-            Backend Code 
-          </a>
-          <i className="uil uil-arrow-right services__button-icon"></i>
-        </h5>
+          </div>
 
+          <div className="box">
+            <div className="box__img-web">
+              <a href="https://fullstack-courses-app-3vdz.vercel.app/" target="_blank">
+                <img src={Courses} alt="Courses Tech App" className="animate__img-web3" />
+              </a>
+            </div>
+            <div className="title__project">
+              <h3 className="pb-2">Courses Tech App</h3>
+              <span>Training Project</span>
+              <div>
+                Technologies: (HTML - CSS - JS - Tailwind - Next.js - Strapi - Stripe - ClerkAuth - Cloudinary)
+              </div>
+              <h5 className="pt-1 d-flex">
+                <a href="https://aljewartours.vercel.app/" target="_blank" rel="noreferrer">
+                  Demo
+                </a>
+                <i className="uil uil-arrow-right services__button-icon me-3"></i>
+              </h5>
+            </div>
           </div>
         </div>
-        {/* React */}
-        <div className={port === 3 ? "portfolio__web-show" : "portfolio__web-hide"} >
+
+        {/* React Projects */}
+        <div className={port === 3 ? "portfolio__web-show" : "portfolio__web-hide"}>
           <div className="box">
             <div className="box__img-react">
-              <img src={finalproject} alt="" />
+              <img src={finalproject} alt="Graduation Project" />
             </div>
             <h4 className="title__project">Graduation Project</h4>
-            <div>
-              Technologies: (HTML - CSS - BootStrap - React - Context api
-              -(react-router-dom)){" "}
-            </div>
+            <div>Technologies: (HTML - CSS - BootStrap - React - Context API - React Router)</div>
             <h5>
-              {" "}
-              <a
-                href="https://ahmedgawish10.github.io/graduateproject/"
-                target="_blank"
-              >
-
+              <a href="https://ahmedgawish10.github.io/graduateproject/" target="_blank" rel="noreferrer">
                 Demo
-              </a>{" "}
+              </a>
             </h5>
           </div>
+
           <div className="box">
             <div className="box__img-react">
-              <img src={ecompharmacy} alt="" />
+              <img src={ecompharmacy} alt="E-commerce Pharmacy" />
             </div>
             <h4 className="title__project">E-commerce Pharmacy</h4>
             <div>
-              Technologies: (HTML - CSS - BootStrap - React - ReduxToolkit
-              -(react-router-dom) - (slick-carousel)){" "}
+              Technologies: (HTML - CSS - BootStrap - React - Redux Toolkit - React Router - Slick Carousel)
             </div>
             <h5>
-              {" "}
-              <a
-                href="https://ecommerce-pharmacy4.vercel.app/"
-                target="_blank"
-              >
-                {" "}
+              <a href="https://ecommerce-pharmacy4.vercel.app/" target="_blank" rel="noreferrer">
                 Demo
-              </a>{" "}
+              </a>
             </h5>
           </div>
         </div>
-        {/* Certificates  */}
-        <div
-          className={
-            port === 4
-              ? "portfolio__web-show certificate"
-              : "portfolio__web-hide"
-          }
-        >
+
+        {/* Certificates */}
+        <div className={port === 4 ? "portfolio__web-show certificate" : "portfolio__web-hide"}>
           <div className="box">
             <div className="box__img-cert">
-              <img src={Image6} alt="" onClick={()=>setimgGrad(!imgGrad)}/>
+              <img src={Image6} alt="ITI Summer Training Certificate" onClick={() => setImgGrad(!imgGrad)} />
             </div>
-            <h4 className="title__certificate">
-              Summer Training Certificate at I T I
-            </h4>
-            <h5 className="content__certificate">
-              Fundamentals Web Development{" "}
+            <h4 className="title__certificate">Summer Training Certificate at I T I</h4>
+            <h5 className="content__certificate">Fundamentals Web Development</h5>
+            <h5 className="view__container">
+              <span className="view_certificate" onClick={() => setImgGrad(!imgGrad)}>
+                View
+              </span>
+              <i className="uil uil-arrow-right services__button-icon"></i>
             </h5>
-            <h5 className="view__container"> <span className="view_certificate" onClick={()=>setimgGrad(!imgGrad)}>View</span><i className="uil uil-arrow-right  services__button-icon"></i> </h5>  
-               <div className={imgGrad==1?"showgrad":"hidegrad"} >
-                <div className="close__img-grad" >
-                <div onClick={()=>setimgGrad(!imgGrad)}>x</div>
-                </div>
-                <div className="box__img-grad">
-                <img src={Image6} alt=""/>
-                </div>
-                </div> 
+            <div className={imgGrad ? "showgrad" : "hidegrad"}>
+              <div className="close__img-grad" onClick={() => setImgGrad(!imgGrad)}>
+                x
+              </div>
+              <div className="box__img-grad">
+                <img src={Image6} alt="ITI Summer Training Certificate" />
+              </div>
+            </div>
           </div>
+
           <div className="box">
             <div className="box__img-cert">
-              <img src={Image6} alt=""  onClick={()=>setimgGrad(!imgGrad)} />
+              <img src={Image6} alt="ITI Web Developer Certificate" onClick={() => setImgGrad(!imgGrad)} />
             </div>
-            <h4 className="title__certificate">
-            Web Developer Certificate at I T I
-            </h4>
-            <h5 className="content__certificate">
-              Fullstack PHP Web Developer
+            <h4 className="title__certificate">Web Developer Certificate at I T I</h4>
+            <h5 className="content__certificate">Fullstack PHP Web Developer</h5>
+            <h5 className="view__container">
+              <span className="view_certificate" onClick={() => setImgGrad(!imgGrad)}>
+                View
+              </span>
+              <i className="uil uil-arrow-right services__button-icon"></i>
             </h5>
-            <h5 className="view__container"> <span className="view_certificate" onClick={()=>setimgGrad(!imgGrad)}>View</span><i className="uil uil-arrow-right  services__button-icon"></i> </h5>  
-               <div className={imgGrad==1?"showgrad":"hidegrad"} >
-                <div className="close__img-grad" >
-                <div onClick={()=>setimgGrad(!imgGrad)}>x</div>
-                </div>
-                <div className="box__img-grad">
-                <img src={Image6} alt=""/>
-                </div>
-                </div> 
+            <div className={imgGrad ? "showgrad" : "hidegrad"}>
+              <div className="close__img-grad" onClick={() => setImgGrad(!imgGrad)}>
+                x
+              </div>
+              <div className="box__img-grad">
+                <img src={Image6} alt="ITI Web Developer Certificate" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
