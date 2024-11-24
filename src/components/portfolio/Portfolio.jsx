d8e073dbb8f150863091e7500fd113f98f83f489
ahmedgawish10 <ahmdgawish44@gmail.com>
2024-11-24 02:41:18 +0200

    update projects

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -44,7 +44,7 @@ const Portfolio = () => {
               <div className="box" key={index}>
                 <div className="box__img-web">
                   <a href={project.link} target="_blank" rel="noreferrer">
-                    <img src={project.img} alt={project.title} loading="lazy" className="rounded-t-xl absolute" />
+                    <img src={project.img} alt={project.title}  className="rounded-t-xl absolute" />
                   </a>
                 </div>
                 <div className="title__project">
@@ -68,7 +68,7 @@ const Portfolio = () => {
             {reactProjects.map((project, index) => (
               <div className="box" key={index}>
                 <div className="box__img-react">
-                  <img src={project.image} alt={project.title} loading="lazy" className="rounded-t-xl absolute" />
+                  <img src={project.image} alt={project.title}  className="rounded-t-xl absolute" />
                 </div>
                 <h4 className="title__project">{project.title}</h4>
                 <h5 className="content__certificate">{project.description}</h5>
@@ -91,7 +91,6 @@ const Portfolio = () => {
               <div className="box" key={index}>
                 <div className="box__img-cert ">
                   <img
-                    loading="lazy"
                     src={certificate.img}
                     alt={certificate.title}
                     className={`cursor-pointer rounded-t-xl absolute ${index == 1 ? "h-full w-full" : ""} `}
